c3fb597d856421e684734ce7d88d3ad5e3701965
markdibarry <markdibarry@protonmail.com>
2023-05-14 10:24:19 -0400

    Handle config changes

--- Arenbee/GUI/Loaders/LoaderFactory.cs
+++ Arenbee/GUI/Loaders/LoaderFactory.cs
@@ -8,7 +8,7 @@ public class LoaderFactory : ILoaderFactory
 {
     public ObjectLoader GetLoader(string path, Action reportCallback)
     {
-        if (path.StartsWith(Config.SavePrefix))
+        if (path.StartsWith(Config.SaveNamePrefix))
             return new GameSaveLoader(path, reportCallback);
         else
             return new ResourceLoader(path, reportCallback);
